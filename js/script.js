309d99b14a896136ae4cdf6a05dd015f851c9a7e
Alexander <ashengolts87@mail.ru>
2024-10-16 15:27:26 +0900

    add web sports

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Menu
+
+function openModal() {
+    document.getElementById("modal").style.top = "0px";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-400px";
+}
+// Button Up
+
+window.onscroll = function () { scrollFunction() };
+
+const upbuttons = document.querySelectorAll(".arrow");
+
+for (const upbutton of upbuttons) {
+    upbutton.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        document.getElementById('btnUp').className = 'arrow';
+    } else {
+        document.getElementById('btnUp').className = 'arrow';
+    }
+}
